ad2ad8c5d2717160549fe1a0877ba0c4c390613c
fahim-khandakar <fahimkhandakar01@gmail.com>
2025-01-25 12:26:51 +0600

    add lucide-react dependency and replace times.svg with icon component in TopBanner

--- src/components/layout/Banner/TopBanner.tsx
+++ src/components/layout/Banner/TopBanner.tsx
@@ -1,31 +1,27 @@
 import { Button } from "@/components/ui/Button/button";
-import Image from "next/image";
+import { X } from "lucide-react";
+
 import Link from "next/link";
 import React from "react";
 
 const TopBanner = () => {
   return (
     <div className="bg-black text-white text-center py-2 px-2 sm:px-4 xl:px-0">
-      <div className="relative max-w-frame mx-auto">
+      <div className="relative max-w-5xl mx-auto">
         <p className="text-xs sm:text-sm">
-          Sign up and get 20% off to your first order.{" "}
+          Sign up and get 20% off to your first order.
           <Link href="#" className="underline font-medium">
             Sign Up Now
           </Link>
         </p>
         <Button
           variant="ghost"
-          className="hover:bg-transparent absolute right-0 top-1/2 -translate-y-1/2 w-fit h-fit p-1 hidden sm:flex"
+          className="text-white hover:bg-transparent absolute right-0 top-1/2 -translate-y-1/2 w-fit h-fit p-1 hidden sm:flex"
           size="icon"
           aria-label="close banner"
+          type="link"
         >
-          <Image
-            priority
-            src="/icons/times.svg"
-            height={13}
-            width={13}
-            alt="close banner"
-          />
+          <X />
         </Button>
       </div>
     </div>
