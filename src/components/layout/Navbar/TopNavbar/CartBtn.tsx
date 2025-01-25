a8b93c447da2d2b6ac9842749d613e9c4df1061d
fahim-khandakar <fahimkhandakar01@gmail.com>
2025-01-25 15:06:06 +0600

    updated navbar

--- src/components/layout/Navbar/TopNavbar/CartBtn.tsx
+++ src/components/layout/Navbar/TopNavbar/CartBtn.tsx
@@ -8,13 +8,13 @@ import React from "react";
 
 const CartBtn = () => {
   // const { cart } = useAppSelector((state: RootState) => state.carts);
-  const cart = { totalQuantities: 0 };
+  const cart = { totalQuantities: 3 };
 
   return (
-    <Link href="/cart" className="relative mr-[14px] p-1">
+    <Link href="/cart" className="relative mr-[14px]">
       <ShoppingCart />
       {cart && cart.totalQuantities > 0 && (
-        <span className="border bg-black text-white rounded-full w-3 px-1 text-xs absolute -top-3 left-1/2 -translate-x-1/2">
+        <span className=" bg-black text-white rounded-full  px-1 text-xs absolute -top-3 left-1/2 translate-x-1/2">
           {cart.totalQuantities}
         </span>
       )}
