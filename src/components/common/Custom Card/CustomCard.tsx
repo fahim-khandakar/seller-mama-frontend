4564b4e25493c6fe7085bdd6abee74afd43a702c
fahim-khandakar <fahimkhandakar01@gmail.com>
2026-04-30 23:30:31 +0600

    done

--- src/components/common/Custom Card/CustomCard.tsx
+++ src/components/common/Custom Card/CustomCard.tsx
@@ -1,10 +1,9 @@
-import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardFooter } from '@/components/ui/card';
-import { ShoppingCart, Star } from 'lucide-react';
+import { ShoppingCart } from 'lucide-react';
 import Image from 'next/image';
-import { Product } from './config/types';
 import { IProduct } from '@/types/product.type';
+import Link from 'next/link';
 
 export default function CustomCard({ product }: { product: IProduct }) {
   return (
@@ -14,12 +13,14 @@ export default function CustomCard({ product }: { product: IProduct }) {
     >
       <CardContent className="p-0 relative aspect-[3/4] rounded-2xl overflow-hidden bg-slate-100 dark:bg-slate-900">
         {/* Product Image */}
-        <Image
-          src={product?.images ? product.images[0] : ''}
-          alt={product.name}
-          fill
-          className="object-cover transition-transform duration-500 group-hover:scale-110"
-        />
+        <Link href={`/shop/${product?._id}`}>
+          <Image
+            src={product?.images ? product.images[0] : ''}
+            alt={product.name}
+            fill
+            className="object-cover transition-transform duration-500 group-hover:scale-110"
+          />
+        </Link>
 
         {/* Badge Overlay */}
         {/* <div className="absolute top-3 left-3 flex flex-col gap-2">
@@ -49,32 +50,37 @@ export default function CustomCard({ product }: { product: IProduct }) {
         </div>
       </CardContent>
 
-      <CardFooter className="flex flex-col items-start px-1 py-4 gap-1">
-        {/* Rating */}
-        {/* <div className="flex items-center gap-1 text-orange-500 mb-1">
+      <Link href={`/shop/${product?._id}`}>
+        <CardFooter className="flex flex-col items-start px-1 py-4 gap-1">
+          {/* Rating */}
+          {/* <div className="flex items-center gap-1 text-orange-500 mb-1">
           <Star className="w-3 h-3 fill-current" />
           <span className="text-xs font-bold text-slate-600 dark:text-slate-400">
             {product.rating}
           </span>
         </div> */}
 
-        {/* Title */}
-        <h3 className="font-bold text-lg text-slate-900 dark:text-white line-clamp-1 group-hover:text-orange-600 transition-colors">
-          {`${product?.name} (${product?.category?.name})`}
-        </h3>
+          {/* Title */}
+          <h3 className="font-bold text-lg text-slate-900 dark:text-white line-clamp-1 group-hover:text-orange-600 transition-colors">
+            {`${product?.name} (${product?.category?.name})`}
+          </h3>
 
-        {/* Price */}
-        <div className="flex items-center gap-3">
-          <span className="text-xl font-black text-slate-900 dark:text-white">
-            ৳{product.discountPrice ? product.discountPrice : product.basePrice}
-          </span>
-          {product.discountPrice && (
-            <span className="text-sm text-slate-400 line-through">
-              ৳{product.basePrice}
+          {/* Price */}
+          <div className="flex items-center gap-3">
+            <span className="text-xl font-black text-slate-900 dark:text-white">
+              ৳
+              {product.discountPrice
+                ? product.discountPrice
+                : product.basePrice}
             </span>
-          )}
-        </div>
-      </CardFooter>
+            {product.discountPrice && (
+              <span className="text-sm text-slate-400 line-through">
+                ৳{product.basePrice}
+              </span>
+            )}
+          </div>
+        </CardFooter>
+      </Link>
     </Card>
   );
 }
